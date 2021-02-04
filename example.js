79819b0710448c56b456112519cf2ebf2c2a2ea7
Ai Tran Xuan <atranxn@Ais-MacBook-Pro.local>
2021-02-04 09:31:57 +0700

    Init branch

---
+++ example.js
@@ -0,0 +1,168 @@
+import { Alert, StyleSheet, Text } from 'react-native'
+import React, { Component } from 'react'
+import { inject, observer } from 'mobx-react'
+
+import FastImage from 'react-native-fast-image';
+import { Icons } from '@constant';
+import TouchablePreventDouble from '@component/touchable-prevent-double';
+import { config } from '@config';
+import { findIndex } from 'lodash'
+import { size } from 'lodash'
+import { toJS } from 'mobx';
+
+@inject('OnApp', 'Search', "Love", 'User')
+@observer
+export default class BottomView extends Component {
+
+    clickCloseCompare = () => {
+        const { Search, OnApp, Love, User } = this.props;
+        Alert.alert('Xoá so sánh', 'Xoá tất cả xe đã chọn ra khỏi danh sách?',
+            [
+                {
+                    text: 'Huỷ', onPress: () => {
+                        console.log('huỷ đăng xuất')
+                    }, style: 'cancel'
+                },
+                {
+                    text: 'Đồng ý', onPress: () => {
+
+                        //Cap nhat trang thai  danh sach man hinh Search
+                        switch (Search.tabScreen) {
+                            case config.postType.all:
+                                OnApp.dataTop.map((item) => {
+                                    console.log('item: ' + JSON.stringify(item))
+                                    let indexCompare = findIndex(Search.dataAll, function (o) {
+                                        return o.id === item.id;
+                                    })
+                                    console.log('indexCompare: ' + indexCompare)
+                                    if (indexCompare !== undefined && indexCompare !== '-1') {
+                                        if (Search.dataAll && Search.dataAll[indexCompare]) {
+                                            Search.dataAll[indexCompare].isCompare = false;
+                                        }
+                                    }
+                                    //yeu thich
+                                    let indexLove = findIndex(Love.listLove, function (o) {
+                                        return o.id === item.id;
+                                    })
+                                    console.log('indexLove: ' + indexLove)
+
+                                    if (indexLove !== undefined && indexLove !== '-1') {
+                                        if (Love.listLove && Love.listLove[indexLove]) {
+                                            Love.listLove[indexLove].isCompare = false;
+                                        }
+                                    }
+                                })
+                                break;
+
+                            case config.postType.new:
+                                OnApp.dataTop.map((item) => {
+                                    let indexCompare = findIndex(Search.dataNew, function (o) {
+                                        return o.id === item.id;
+                                    })
+                                    if (indexCompare !== undefined && indexCompare !== '-1') {
+                                        if (Search.dataNew && Search.dataNew[indexCompare]) {
+                                            Search.dataNew[indexCompare].isCompare = false;
+                                        }
+                                    }
+                                    //yeu thich
+                                    let indexLove = findIndex(Love.listLove, function (o) {
+                                        return o.id === item.id;
+                                    })
+
+                                    if (indexLove !== undefined && indexLove !== '-1') {
+                                        if (Love.listLove && Love.listLove[indexLove]) {
+                                            Love.listLove[indexLove].isCompare = false;
+                                        }
+                                    }
+                                })
+                                break;
+
+                            case config.postType.old:
+                                OnApp.dataTop.map((item) => {
+
+                                    //Tim kiem
+                                    let indexCompare = findIndex(Search.dataOld, function (o) {
+                                        return o.id === item.id;
+                                    })
+                                    if (indexCompare !== undefined && indexCompare !== '-1') {
+                                        if (Search.dataOld && Search.dataOld[indexCompare]) {
+                                            Search.dataOld[indexCompare].isCompare = false;
+                                        }
+                                    }
+
+                                    //yeu thich
+                                    let indexLove = findIndex(Love.listLove, function (o) {
+                                        return o.id === item.id;
+                                    })
+
+                                    if (indexLove !== undefined && indexLove !== '-1') {
+                                        if (Love.listLove && Love.listLove[indexLove]) {
+                                            Love.listLove[indexLove].isCompare = false;
+                                        }
+                                    }
+
+                                })
+                                break;
+
+                            default:
+                                break;
+                        }
+
+                        //xoa tat ca 
+                        OnApp.clearListCarCompare()
+                        OnApp.removeCompare(User.userInfo.id || '', '', 1, () => {//0: xoa 1 xe, 1: xoa tat ca xe khoi danh sach so sanh
+                        })
+
+                        //tim kiem cac item trong tat ca danh sach ròi xoá
+                    }, style: 'destructive'
+                },
+            ],
+        )
+
+    }
+
+    render() {
+        const { OnApp, clickCompareCar } = this.props
+        const dataCheck = toJS(OnApp.dataTop)
+        return (
+            dataCheck && size(dataCheck) > 0
+                ?
+                <TouchablePreventDouble
+                    onPress={clickCompareCar}
+                    activeOpacity={1}
+                    style={styles.v1}>
+                    <FastImage
+                        style={styles.v2}
+                        resizeMode='contain'
+                        tintColor='#fff'
+                        source={Icons.ic_compare} />
+                    <Text style={styles.v3}>
+                        {'So sánh ' + size(dataCheck) + ' xe ô tô'}
+                    </Text>
+                    <TouchablePreventDouble
+                        onPress={this.clickCloseCompare}
+                        activeOpacity={1}
+                        style={styles.v4}>
+                        <FastImage
+                        resizeMode='contain'
+                        tintColor='#fff'
+                            style={styles.v5}
+                            source={Icons.ic_close} />
+                    </TouchablePreventDouble>
+                </TouchablePreventDouble>
+                :
+                null
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    v5: { width: 20, height: 20 },
+    v4: { width: 40, height: 40, justifyContent: 'center', alignItems: 'center' },
+    v3: { color: '#fff', flex: 1, fontSize: 13, },
+    v2: { width: 20, height: 20, marginHorizontal: 10, },
+    v1: {
+        position: 'absolute', flex: 1, width: '100%', flexDirection: 'row', justifyContent: 'space-between',
+        bottom: 0, backgroundColor: '#3C4146', height: 40, alignItems: 'center'
+    },
+})
---
+++ house.js
@@ -0,0 +1,138 @@
+import { Col, RowTop, Table, TableWrapper, } from '@react-native-table-component';
+import React, { Component } from 'react'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import { inject, observer, } from 'mobx-react'
+
+import ChangeTypeCompare from './navbar/ChangeTypeCompare';
+import NavbarCompareCar from './navbar/NavbarCompareCar';
+import { Navigation } from 'react-native-navigation';
+import { color } from '@config';
+
+@inject('OnApp')
+@observer
+export default class CompareCar extends Component {
+    clickLeftButton = () => {
+        Navigation.pop(this.props.componentId);
+    }
+
+    changeType = () => {
+        const { OnApp } = this.props;
+        OnApp.showTypeSelected()
+    };
+
+    clickItem = (item) => {
+        const { OnApp } = this.props;
+        OnApp.setTypeSelected(item)
+        this.dismissModal()
+    }
+
+    dismissModal = () => {
+        const { OnApp } = this.props;
+        OnApp.hiddenTypeSelected()
+    }
+
+    componentWillUnmount() {
+        const { OnApp } = this.props;
+        OnApp.clearTypeSelected()
+    }
+
+    render() {
+        const { OnApp } = this.props;
+        return (
+            <View style={{ width: '100%', flex: 1 }}>
+                <NavbarCompareCar {...this.props}
+                    clickLeftButton={this.clickLeftButton}
+                    title={OnApp.typeCompare && OnApp.typeSelected.value || ''}
+                    changeType={this.changeType} />
+                <ScrollView
+                    keyboardShouldPersistTaps='handled'
+                    style={styles.a1}>
+                    {/* Content so sánh xe */}
+                    <View style={styles.container}>
+                        <ScrollView
+                            keyboardShouldPersistTaps='handled'
+                            horizontal style={styles.a2}>
+                            <View style={styles.a2}>
+                                <Table
+                                    borderStyle={styles.a3}>
+                                    {
+                                        <RowTop
+                                            data={OnApp.dataTop}
+                                            // widthArr={200}
+                                            style={[styles.row]}
+                                            textStyle={styles.text}
+                                            {...this.props}
+                                        />
+                                    }
+                                </Table>
+                                <View style={styles.a4}>
+                                    <Text style={styles.a5}>{'Thông tin chung'}</Text>
+                                </View>
+                                <ScrollView
+                                    keyboardShouldPersistTaps='handled'
+                                    scrollEnabled={false}
+                                    // horizontal
+                                    style={styles.dataWrapper}>
+                                    <TableWrapper
+
+                                        style={styles.a6}
+                                        borderStyle={styles.a7}
+                                    >
+                                        {
+                                            OnApp.typeSelected.id === 0
+                                                ?
+                                                OnApp.dataContent.map((item, i) => {
+                                                    // console.log('dt content: ' + JSON.stringify(item))
+                                                    return <Col
+                                                        key={i}
+                                                        data={item}
+                                                        isShowTitle={i === 0 ? true : false}
+                                                        style={styles.head} heightArr={[60, 60]}
+                                                        textStyle={styles.text}
+                                                        {...this.props}
+                                                    />
+                                                })
+                                                :
+                                                OnApp.dataContent_Different.map((item, i) => {
+                                                    // console.log('dt dataContent_Different: ' + JSON.stringify(item))
+                                                    return <Col
+                                                        key={i}
+                                                        data={item}
+                                                        isShowTitle={i === 0 ? true : false}
+                                                        style={styles.head} heightArr={[60, 60]}
+                                                        textStyle={styles.text}
+                                                        {...this.props}
+                                                    />
+                                                })
+                                        }
+                                    </TableWrapper>
+                                </ScrollView>
+                            </View>
+                        </ScrollView>
+                    </View>
+                </ScrollView>
+
+                {/* Chọn loại hiển thị của loại so sánh */}
+                <ChangeTypeCompare clickItem={this.clickItem} />
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    a7: { borderColor: '#C1C0B9' },
+    a6: { flexDirection: 'row', flex: 1 },
+    a5: { color: 'black', fontWeight: 'bold', fontSize: 15, },
+    a4: { justifyContent: 'center', width: '100%', height: 40, paddingHorizontal: 10, borderBottomWidth: 0.5, borderBottomColor: color.borderNavbar },
+    a3: { borderColor: color.borderNavbar },
+    a2: { flex: 1, },
+    a1: { flex: 1, width: '100%', },
+    container: { flex: 1, width: '100%', flexDirection: 'row', backgroundColor: 'transparent', },
+    // header: { height: 30, },
+    // textHeader: { textAlign: 'center', color: 'white', fontSize: 11, },
+    // textHeaderContent: { textAlign: 'center', color: 'white', fontWeight: '500', },
+    text: { textAlign: 'center', color: 'white' },
+    dataWrapper: { marginTop: -1, flex: 1, width: '100%' },
+    // wrapper: { flexDirection: 'row', }
+    row: { backgroundColor: 'transparent' }
+});
